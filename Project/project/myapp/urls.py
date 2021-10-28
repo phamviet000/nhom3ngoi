748675ded6ecf64c8fe6731ddc411b3cecabf038
phamviet000 <74182334+phamviet000@users.noreply.github.com>
2021-10-29 01:38:08 +0700

    upload
    fix bug login

--- Project/project/myapp/urls.py
+++ Project/project/myapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-
+from .views import LoginClass
 app_name = 'myapp'
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='home'),
     path('login/', LoginClass.as_view(), name='login'),
+    path('logout/', views.logoutuser, name='logout'),
 ]
--- Project/project/project/settings.py
+++ Project/project/project/settings.py
@@ -135,5 +135,5 @@ DEFAULT_FROM_EMAIL='pcapabe@gmail.com'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-AUTH_USER_MODEL = "myapp.MyUser"
+#AUTH_USER_MODEL = "myapp.MyUser"
 
