1fe88e35e2a54d696586cd5dcb223808ca6d6d0a
phamviet000 <74182334+phamviet000@users.noreply.github.com>
2021-10-28 22:50:27 +0700

    upload
    chỉnh sửa link

--- Project/project/myapp/models.py
+++ Project/project/myapp/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
-class MyUser(AbstractUser):
-    phone_number = models.CharField(('number_phone'),max_length=15,blank=True)
-    birdth_day = models.DateField(('birdth_day'),null=True,blank=True)
-    sex = models.CharField(('sex'),max_length=15,blank=True)
-
-    # def __str__(self):
-    #     return self.user.username
+# class MyUser(AbstractUser):
+#     phone_number = models.CharField(('number_phone'),max_length=15,blank=True)
+#     birdth_day = models.DateField(('birdth_day'),null=True,blank=True)
+#     sex = models.CharField(('sex'),max_length=15,blank=True)
+#
+#     # def __str__(self):
+#     #     return self.user.username
