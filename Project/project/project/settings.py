2b26f1f168849ffd144acb8512bdb8c3aa4ea449
phamviet000 <74182334+phamviet000@users.noreply.github.com>
2021-11-04 13:10:26 +0700

    upload
    fix heroku

--- Project/project/project/settings.py
+++ Project/project/project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,4 +137,5 @@ DEFAULT_FROM_EMAIL='pcapabe@gmail.com'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = "myapp.MyUser"
+django_heroku.settings(locals())
 
