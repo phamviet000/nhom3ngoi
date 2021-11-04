b1039e86ded4d02a796e0cab5314a3fb2038f7cf
phamviet000 <74182334+phamviet000@users.noreply.github.com>
2021-11-04 13:01:00 +0700

    upload
    ket noi heroku

--- Project/project/project/settings.py
+++ Project/project/project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-khx9mrfxhuvbjk99vn!!6yzbdeal@nf)1ungfvv65sgd^hg#vm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nhom3ngoi.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
