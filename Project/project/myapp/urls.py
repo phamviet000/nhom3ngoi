e758841e0127d84e517a5da38be1c203a8da6f77
phamviet000 <74182334+phamviet000@users.noreply.github.com>
2021-11-06 22:11:39 +0700

    upload
    connect to Profile and Blog

--- Project/project/myapp/urls.py
+++ Project/project/myapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-from .views import LoginClass,RegistrationView,ActivateAccountView,ViewUser,edit_profile
+from .views import LoginClass,RegistrationView,ActivateAccountView,ViewUser,edit_profile,blog
 app_name = 'myapp'
 urlpatterns = [
     path('', views.index, name='home'),
+    path('blog', blog.as_view(), name='blog'),
     path('login/', LoginClass.as_view(), name='login'),
     path('logout/', views.logoutuser, name='logout'),
     path('signup/',RegistrationView.as_view(),name='signup'),
